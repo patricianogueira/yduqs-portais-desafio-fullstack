c9777930ac65a37154924f1af9845c4d3e3fd354
Patricia Nogueira de Santana Silva <snogueira.patricia@gmail.com>
2025-11-13 03:11:28 -0300

    feat: fix cors

--- cadastro-api/src/main.ts
+++ cadastro-api/src/main.ts
@@ -26,6 +26,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document); 
 
+  app.enableCors({
+    origin: 'http://localhost:3001', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
